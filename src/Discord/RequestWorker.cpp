93810772fd7633264b1dede11177b9effba43d2e
ouwou <26526779+ouwou@users.noreply.github.com>
2026-09-11 01:19:05 -0400

    set origin on non-gets

--- src/Discord/RequestWorker.cpp
+++ src/Discord/RequestWorker.cpp
@@ -192,6 +192,8 @@ CURL *RequestWorker::buildEasyHandle(TransferContext *ctx)
     curl_easy_setopt(curl, CURLOPT_SHARE, share);
 
     curl_slist *headers = nullptr;
+    if (desc.method != HttpClient::Method::GET)
+        headers = curl_slist_append(headers, "Origin: https://discord.com");
     if (desc.external) {
         if (!desc.contentType.isEmpty())
             headers = curl_slist_append(headers, ("Content-Type: " + desc.contentType).toUtf8().constData());
